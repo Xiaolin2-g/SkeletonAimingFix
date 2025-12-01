d96d7ff567ab76c34b3dddfa4a4e06e9dfed31b8
Xiaolin2 <79189625+Xiaolin2-g@users.noreply.github.com>
2025-12-01 02:23:38 -0300

    mod works on forge

--- forge/src/main/java/com/xiaolin/skeletonaimingfix/mixin/SkeletonAimingFixMixin.java
+++ forge/src/main/java/com/xiaolin/skeletonaimingfix/mixin/SkeletonAimingFixMixin.java
@@ -1,6 +1,7 @@
 package com.xiaolin.skeletonaimingfix.mixin;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.RangedBowAttackGoal;
 import net.minecraft.world.entity.monster.Monster;
 
@@ -13,18 +14,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 
-@Mixin(RangedBowAttackGoal.class)
+@Mixin(net.minecraft.world.entity.ai.goal.RangedBowAttackGoal.class)
 public abstract class SkeletonAimingFixMixin {
 
     @Shadow @Final
-    private Monster mob;
+    private Mob mob;
 
-    @Inject(method = "tick",
-            at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/monster/Monster;lookAt(Lnet/minecraft/world/entity/Entity;FF)V",
-                    shift = At.Shift.AFTER),
+    @Inject(method = "tick", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/Mob;lookAt(Lnet/minecraft/world/entity/Entity;FF)V",
+            shift = At.Shift.AFTER),
             locals = LocalCapture.CAPTURE_FAILSOFT)
-
     public void skeletonAimingFix(CallbackInfo ci, LivingEntity livingEntity){
         this.mob.getLookControl().setLookAt(livingEntity, 30.0f, 30.0f);
     }
--- forge/src/main/java/com/xiaolin/skeletonaimingfix/platform/ForgePlatformHelper.java
+++ forge/src/main/java/com/xiaolin/skeletonaimingfix/platform/ForgePlatformHelper.java
@@ -1,6 +1,6 @@
-package com.example.examplemod.platform;
+package com.xiaolin.skeletonaimingfix.platform;
 
-import com.example.examplemod.platform.services.IPlatformHelper;
+import com.xiaolin.skeletonaimingfix.platform.services.IPlatformHelper;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 
